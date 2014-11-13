6980e06fec4c80d785409741056967f48cc45a2a
Min RK <benjaminrk@gmail.com>
2014-11-12 22:08:04 -0800

    add `--no-include-prefix`
    aka `includePrefix: false`
    
    if set, strips the routing prefix from the forwarded request URL.
    
    implements feature requested in #19

--- test/proxy_spec.js
+++ test/proxy_spec.js
@@ -64,15 +64,14 @@ describe("Proxy Tests", function () {
         });
     });
     
-    it("target path is prepended", function (done) {
+    it("target path is prepended by default", function (done) {
         util.add_target(proxy, '/bar', port + 10, false, '/foo');
-        r(proxy_url + '/bar', function (error, res, body) {
+        r(proxy_url + '/bar/rest/of/it', function (error, res, body) {
             expect(res.statusCode).toEqual(200);
             body = JSON.parse(body);
-            console.log(body);
             expect(body).toEqual(jasmine.objectContaining({
                 path: '/bar',
-                url: '/foo/bar'
+                url: '/foo/bar/rest/of/it'
             }));
             done();
         });
@@ -81,13 +80,41 @@ describe("Proxy Tests", function () {
     it("prependPath: false prevents target path from being prepended", function (done) {
         proxy.proxy.options.prependPath = false;
         util.add_target(proxy, '/bar', port + 10, false, '/foo');
-        r(proxy_url + '/bar', function (error, res, body) {
+        r(proxy_url + '/bar/rest/of/it', function (error, res, body) {
             expect(res.statusCode).toEqual(200);
             body = JSON.parse(body);
-            console.log(body);
             expect(body).toEqual(jasmine.objectContaining({
                 path: '/bar',
-                url: '/bar'
+                url: '/bar/rest/of/it'
+            }));
+            done();
+        });
+    });
+    
+    it("includePrefix: false strips routing prefix from request", function (done) {
+        proxy.includePrefix = false;
+        util.add_target(proxy, '/bar', port + 10, false, '/foo');
+        r(proxy_url + '/bar/rest/of/it', function (error, res, body) {
+            expect(res.statusCode).toEqual(200);
+            body = JSON.parse(body);
+            expect(body).toEqual(jasmine.objectContaining({
+                path: '/bar',
+                url: '/foo/rest/of/it'
+            }));
+            done();
+        });
+    });
+    
+    it("includePrefix: false + prependPath: false", function (done) {
+        proxy.includePrefix = false;
+        proxy.proxy.options.prependPath = false;
+        util.add_target(proxy, '/bar', port + 10, false, '/foo');
+        r(proxy_url + '/bar/rest/of/it', function (error, res, body) {
+            expect(res.statusCode).toEqual(200);
+            body = JSON.parse(body);
+            expect(body).toEqual(jasmine.objectContaining({
+                path: '/bar',
+                url: '/rest/of/it'
             }));
             done();
         });
